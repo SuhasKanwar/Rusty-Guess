d24ab3a3964dc228699dff79e0d7885f1b90667e
SuhasKanwar <suhas.kanwar@gmail.com>
2025-02-17 21:40:57 +0530

    Generated random number and took the input from the user:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Rusty Guess!");
+    println!("The game of guessing a number between 1 and 100.\n\n");
+
+    let secret_number: i32 = rand::rng().random_range(1..=100);
+
+    println!("Generated secret number {secret_number}");
+
+    println!("Please enter your guess:");
+    let mut guess: String = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read the user input");
+
+    println!("You guessed {guess}");
 }
